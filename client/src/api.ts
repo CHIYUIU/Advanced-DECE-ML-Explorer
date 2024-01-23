be5fe30dc474b37e899626fd4737bc7207bfa719
CHIYUIU <117708554+CHIYUIU@users.noreply.github.com>
2024-01-23 20:44:26 +0000

    drm/radeon: only apply hdmi bpc pll flags when encoder mode is hdmi

--- client/src/api.ts
+++ client/src/api.ts
@@ -5,4 +5,62 @@ import { ROOT_URL, DEV_MODE } from "./env";
 
 const API = `${ROOT_URL}/api`;
 
-function checkRespons
+function checkResponse<T>(response: AxiosResponse<T>, fallback: T): T {
+  if (response.status === 200) return response.data;
+  console.error(`Data fetching error: ${response.status}`);
+  if (DEV_MODE) {
+    console.error(response);
+    throw response;
+  }
+  return fallback;
+}
+
+export async function getData(params: { dataId: string }): Promise<string[][]> {
+  const url = `${API}/data`;
+  const response = await axios.get(url, { params });
+  const data = checkResponse(response, []);
+  const dataset = d3.csvParseRows(data);
+  return dataset;
+}
+
+export async function getDataMeta(params: {
+  dataId: string;
+  modelId?: string;
+}): Promise<DataMeta> {
+  const url = `${API}/data_meta`;
+  const response = await axios.get(url, { params });
+  const data = checkResponse(response, []);
+  return new DataMeta(data);
+}
+
+export async function getCFMeta(params: {
+  dataId: string;
+  modelId?: string;
+}): Promise<DataMeta> {
+  const url = `${API}/cf_meta`;
+  const response = await axios.get(url, { params });
+  const data = checkResponse(response, []);
+  return new DataMeta(data);
+}
+
+export async function getDataset(params: {
+  dataId: string;
+  modelId?: string;
+}): Promise<Dataset> {
+  let data = await getData(params);
+  const dataMeta = await getDataMeta(params);
+  const dataFrame = buildDataFrame(dataMeta, data);
+  return new Dataset(dataFrame, dataMeta);
+}
+
+export type CounterFactual = (string | number)[];
+
+export type Filter = {
+  name: string,
+  extent?: [number, number],
+  categories?: string[]
+}
+
+export interface CFResponse {
+  index: number;
+  counterfactuals: C
