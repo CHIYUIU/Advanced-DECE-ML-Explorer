fdc3435d260aac9db884d02266ebb25c89301e4d
CHIYUIU <117708554+CHIYUIU@users.noreply.github.com>
2024-01-30 08:25:33 +0000

    maj

--- client/src/components/CompactTable/table_state.ts
+++ client/src/components/CompactTable/table_state.ts
@@ -22,4 +22,63 @@ export interface ExpandedRow {
   cfLoaded?: boolean;
 }
 
-export function isExpandedRow(row: CollapsedRows | ExpandedRow): row is ExpandedRow 
+export function isExpandedRow(row: CollapsedRows | ExpandedRow): row is ExpandedRow {
+  return row.state === RowStateType.EXPANDED;
+}
+
+export type RowState = CollapsedRows | ExpandedRow;
+
+export function initRowStates (nRows: number): RowState[] {
+  return breakCollapsedRows({startIndex: 0, endIndex: nRows, state: RowStateType.COLLAPSED});
+}
+
+// Actions
+
+export enum ActionType {
+  COLLAPSE_ROWS = 'COLLAPSE_ROWS',
+  EXPAND_ROWS = 'EXPAND_ROWS',
+  REORDER_ROWS = 'REORDER_ROWS',
+  FILTER_ROWS = 'FILTER_ROWS',
+  SORT = 'SORT',
+  FILTER = 'FILTER',
+  UPDATE_COLUMNS = 'UPDATE_COLUMNS',
+}
+
+export interface Action {
+  type: ActionType;
+}
+
+export interface CollapseRows extends Action {
+  startIndex: number;
+  endIndex: number;
+  type: ActionType.COLLAPSE_ROWS;
+}
+
+export interface ExpandRows extends Action {
+  startIndex: number;
+  endIndex: number;
+  dataIndex: number[];
+  type: ActionType.EXPAND_ROWS;
+}
+
+export interface ReorderRows extends Action {
+  type: ActionType.REORDER_ROWS;
+  index: number[];
+}
+
+export interface FilterRows extends Action {
+  type: ActionType.FILTER_ROWS;
+  index: number[];
+}
+
+// Reducers
+
+// break collapsed rows so that we won't render too many rows in a cell
+function breakCollapsedRows(row: CollapsedRows, maxStep: number = 200): CollapsedRows[] {
+  const {startIndex, endIndex, state} = row;
+  const n = Math.ceil((endIndex - startIndex) / maxStep);
+  if (n === 1) return [row];
+  const step = Math.ceil((endIndex - startIndex) / n);
+  const rows: CollapsedRows[] = [];
+  for (let i = 0; i < n; ++i) {
+    const start = startInd
