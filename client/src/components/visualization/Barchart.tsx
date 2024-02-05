089cfd554998c18bcf522bf63336708c11ca6bcd
CHIYUIU <117708554+CHIYUIU@users.noreply.github.com>
2024-02-05 10:29:46 +0000

    fixed import bug

--- client/src/components/visualization/Barchart.tsx
+++ client/src/components/visualization/Barchart.tsx
@@ -32,4 +32,77 @@ export interface IBarChartOptions extends ChartOptions {
   direction: 'up' | 'down',
   renderShades?: boolean,
   drawAxis?: boolean,
-  twisty? :(idx: number) =>
+  twisty? :(idx: number) => number;
+}
+
+export const defaultOptions: IBarChartOptions = {
+  width: 300,
+  height: 200,
+  margin: 3,
+  innerPadding: 0.25,
+  // maxStep: 35,
+  color: defaultCategoricalColor,
+  direction: 'up'
+};
+
+export function drawBarChart(params: {
+  svg: SVGElement,
+  // data: Category[],
+  data: string[] | string[][],
+  allData?: string[] | string[][],
+  dmcData?: string[] | string[][],
+  options?: Partial<IBarChartOptions>}
+) {
+  const {svg, data, allData, dmcData, options} = params;
+  const opts = { ...defaultOptions, ...options };
+  const {
+    height,
+    width,
+    rectStyle,
+    selectedCategories,
+    onSelectCategories,
+    color,
+    xScale,
+    direction,
+    renderShades,
+    drawAxis,
+    twisty
+  } = opts;
+
+  const margin = getMargin(opts.margin);
+  const root = d3.select(svg);
+
+  // const y = d3
+  //   .scaleLinear()
+  //   .range(yRange)
+  //   .domain([0, d3.max(allData || data, d => d.count) as number]);
+
+  const layout = new BarChartLayout({
+    data: data,
+    dmcData: allData,
+    width: width,
+    height: height,
+    mode: "side-by-side",
+    margin: margin,
+    xScale: xScale,
+    direction
+  })
+
+  const bins = layout.layout;
+
+  const AllDatalayout = allData && new BarChartLayout({
+    data: allData,
+    dmcData: allData,
+    width: width,
+    height: height,
+    mode: "side-by-side",
+    margin: margin,
+    xScale: xScale,
+    direction
+  })
+
+  const allBins = AllDatalayout && AllDatalayout.layout;
+
+  const yRange = layout.yRange;
+
+  let _selectedCategories: string[] | undefined = 
