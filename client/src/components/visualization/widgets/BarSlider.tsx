21b8cfb4a1abf026d78137d859e4c1189aeccc24
CHIYUIU <117708554+CHIYUIU@users.noreply.github.com>
2024-02-16 08:27:28 +0000

    sync parent

--- client/src/components/visualization/widgets/BarSlider.tsx
+++ client/src/components/visualization/widgets/BarSlider.tsx
@@ -33,4 +33,44 @@ export interface BarSliderState {
 
 export class BarSlider extends React.Component<BarSliderProps, BarSliderState>{
     private barRef: React.RefObject<SVGGElement> = React.createRef();
-    private sliderRef: Reac
+    private sliderRef: React.RefObject<SVGGElement> = React.createRef();
+    constructor(props: BarSliderProps) {
+        super(props);
+        const {defaultValue, defaultBarActivation, column, xScale} = this.props;
+        this.state = {
+            instanceValue: defaultValue?defaultValue: xScale.domain()[0],
+            barActivation: defaultBarActivation?defaultBarActivation: xScale.domain().map(d => true)
+        }
+
+        this.onBarClicked = this.onBarClicked.bind(this);
+        this.onBarSelected = this.onBarSelected.bind(this);
+        this.onValueChange =  this.onValueChange.bind(this);
+        this.drawAll = this.drawAll.bind(this);
+    }
+
+    render(){
+        const {className, height, width, svgStyle, style} = this.props;
+
+        return <div className={(className || "") + " histslides"} style={{ ...style, width: width + 250 }}>
+        <div style={{ width: width, float: "left" }}>
+            <svg
+                style={svgStyle}
+                width={width}
+                height={height}
+                className='hist-slider'
+            >
+                <g ref={this.barRef} />
+                <g ref={this.sliderRef} />
+            </svg>
+        </div>
+    </div>
+    }
+
+    componentDidMount(){
+        this.drawAll();
+    }
+    componentDidUpdate(prevProps: BarSliderProps){
+        const {column, defaultValue, xScale, defaultBarActivation} = this.props;
+        if (column.name !== prevProps.column.name) {
+            this.setState({
+                instanceValue: defaultValue?defa
