890f164f7b7778c61895aef1e96a2b3339e12b36
CHIYUIU <117708554+CHIYUIU@users.noreply.github.com>
2024-02-16 22:46:46 +0000

    -- cleanup

--- client/src/components/visualization/widgets/checkBox.tsx
+++ client/src/components/visualization/widgets/checkBox.tsx
@@ -29,4 +29,44 @@ const defaultCheckBoxOption: CheckBoxOptions = {
     rx: 1,
     ry: 1,
     defaultValue: true,
-    onClick: 
+    onClick: () => { },
+}
+
+export function d3CheckBox(
+    options: Partial<CheckBoxOptions>,
+) {
+    const { width, height, x, y, rx, ry, defaultValue, onClick } = { ...defaultCheckBoxOption, ...options };
+    const margin = getMargin(options.margin || defaultCheckBoxOption.margin);
+    let checked = defaultValue;
+
+    function checkBox(rootEle: SVGGElement){
+        const root = d3.select(rootEle);
+        const base = getChildOrAppend(root, "g", "check-box-base")
+            .attr("transform", `translate(${margin.left}, ${margin.top})`)
+            .on("click", (d, i, n) => {
+                checked = !checked;
+                getChildOrAppend(d3.select(n[i]), "path", "check-box-mark")
+                    .classed("check-box-mark-checked", checked)
+                onClick();
+            });
+
+        const box = getChildOrAppend(base, "rect", "check-box")
+            .attr("width", width)
+            .attr("height", height)
+            .attr("x", x)
+            .attr("y", y)
+            .attr("rx", rx)
+            .attr("ry", ry);
+            
+        const coordinates: [number, number][] = [
+            [x + width / 8, y + height / 3],
+            [x + width / 2.2, y + height * 3 / 4],
+            [x + width * 7 / 8, y + height / 10],
+        ]
+
+        const line = d3.line();
+
+        const mark = getChildOrAppend(base, "path", "check-box-mark")
+            .attr("d", line(coordinates)!)
+            .classed("check-box-mark-checked", checked)
+    }
